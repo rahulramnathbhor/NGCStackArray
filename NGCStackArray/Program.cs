70806005d346450aa41d6e0feaa594d0f8f6a68e
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-22 16:21:48 +0530

    Add project files.

---
+++ NGCStackArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGCStackArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push(10);
+            stack.Push(20); 
+            stack.Push(30);
+            stack.Push(50);
+            //stack.Pop();
+            stack.Peek();
+            
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
